bd53a660274262c28eeddf8130318901133df59d
YUKIKO IKEDA <applelele@gmail.com>
2020-06-07 23:40:12 +0000

    Automatically backed up by Learn

--- conversions.rb
+++ conversions.rb
@@ -3,6 +3,6 @@ def ounces_to_grams(ounces)
   ounces.to_f * GRAMS_PER_OUNCE
 end
 
-def meal_choice(arg1, arg2)
-  "A plate of meat with broccoli and macaroni."
+def meal_choice(veg, starch, protain="meat")
+  "A plate of #{protain} with #{veg} and #{starch}."
 end
