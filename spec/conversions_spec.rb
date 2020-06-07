188fcff81911a94be101c97fb37a3582a4f5d239
YUKIKO IKEDA <applelele@gmail.com>
2020-06-07 23:34:50 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -8,3 +8,9 @@ describe "conversions" do
     end
   end
 end
+
+describe "#meal_choice" do
+  if "should default to meat for the protain" do
+    expect(meal_choice("broccoli", "macaroni")).to_eq("A plate of meat with broccoli and macaroni.")
+  end
+end
